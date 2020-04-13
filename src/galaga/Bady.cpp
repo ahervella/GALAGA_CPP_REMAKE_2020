8addfac0242e2845e4e9e73c7e3bce61f8edc8f7
Karina Sinha <ksinha1224@gmail.com>
2020-04-12 23:30:44 -0400

    Started extension (enemy movement)
    Enemies disappear a second after moving?

--- src/galaga/Bady.cpp
+++ src/galaga/Bady.cpp
@@ -1,15 +1,35 @@
 #include "Bady.hpp"
+#include <math.h>
 
 Bady::Bady() : GameObject() {}
 
-Bady::Bady(Vector3D pos, Vector3D dim, std::string spritesheetFile, SDL_Renderer* gRenderer) :
+Bady::Bady(Vector3D pos, Vector3D dim, bool m, std::string spritesheetFile, SDL_Renderer* gRenderer) :
 		GameObject(pos, dim, Constants::Galaga::Game::Tag::BADY_TAG, spritesheetFile, gRenderer)
 {
+	mobile = m;
+	populateLocations();
+	if (m)
+	{
+		mForward = true;
+		locationIndex = 1;
+		mStep = 0;
+
+		m0x = ds[0].first - pos.x;
+		m0y = ds[0].second - pos.y;
+
+		float len = sqrtf(m0x + m0x + m0y + m0y);
+		m0x = m0x / len * speed;
+		m0y = m0y / len * speed;
+
+		mSteps = len / speed;
+	}
+
 }
 
 Bady::Bady(Vector3D pos, Vector3D dim) :
 		GameObject(pos, dim, Constants::Galaga::Game::Tag::BADY_TAG)
 {
+	mobile = false;
 }
 
 Bady::~Bady()
@@ -17,6 +37,49 @@ Bady::~Bady()
 }
 
 void Bady::update() {
+
+	if (mobile)
+	{
+		pos.x = (pos.x + m0x * (mForward? 1:-1));
+		pos.y = (pos.y + m0y * (mForward? 1:-1));
+
+		mStep++;
+		if (mStep >= mSteps && locationIndex < ds.size())
+		{
+			m0x = ds[locationIndex - 1].first - pos.x;
+			m0y = ds[locationIndex - 1].second - pos.y;
+
+			float len = sqrtf(m0x + m0x + m0y + m0y);
+			m0x = m0x / len * speed;
+			m0y = m0y / len * speed;
+
+			mSteps = len / speed;
+
+			mStep = 0;
+			locationIndex++;
+
+			std::cout << "Loop 1" << std::endl;
+		}
+
+		else if (mStep >= mSteps && locationIndex >= ds.size())
+		{
+			m0x = ds[0].first - pos.x;
+			m0y = ds[0].second - pos.y;
+
+			float len = sqrtf(m0x + m0x + m0y + m0y);
+			m0x = m0x / len * speed;
+			m0y = m0y / len * speed;
+
+			mSteps = len / speed;
+
+			mStep = 0;
+			locationIndex = 1;
+
+			std::cout << "Loop 2" << std::endl;
+		}
+	}
+
+
 	//TODO: Make enemy more interesting
 	if(!bulletFired) {
 
@@ -46,4 +109,15 @@ bool Bady::collisionUpdate(GameObject::SIDE collision, int otherTag) {
 				}
 	}
 	return false;
+}
+
+void Bady::populateLocations()
+{
+	auto loc = std::make_pair<int, int>(10, 10);
+	auto loc2 = std::make_pair<int, int>(20, 15);
+	auto loc3 = std::make_pair<int, int>(0, 0);
+
+	ds.push_back(loc);
+	ds.push_back(loc2);
+	ds.push_back(loc3);
 }
--- src/galaga/Bady.hpp
+++ src/galaga/Bady.hpp
@@ -3,6 +3,8 @@
 
 #include "GameObject.hpp"
 #include "Constants.hpp"
+#include <vector>
+#include <utility>
 
 /**
  * A bady enemy object in the galaga game
@@ -12,7 +14,7 @@ class Bady : public GameObject {
 public:
 	Bady();
 
-	Bady(Vector3D pos, Vector3D dim, std::string spritesheetFile, SDL_Renderer* gRenderer);
+	Bady(Vector3D pos, Vector3D dim, bool m, std::string spritesheetFile, SDL_Renderer* gRenderer);
 
 	//Used for testing
 	Bady(Vector3D pos, Vector3D dim);
@@ -40,12 +42,27 @@ public:
 	 */
 	bool collisionUpdate(GameObject::SIDE collisionDirection, int otherTag);
 
+	//adds locatiosn for enemies to travel to
+	void populateLocations();
+
 private:
 	//Frames for cooldown of firing
 	float cooldown = 180;
 	float sinceLastShot = 0;
 	bool bulletFired = false;
 
+	///variables to handle movement
+	bool mobile;
+	bool mForward;
+	int mStep;
+	int mSteps;
+	unsigned int locationIndex;
+
+	float m0x;
+	float m0y;
+
+	std::vector<std::pair<int, int>> ds;
+	int speed = 1.5f;
 };
 
 
